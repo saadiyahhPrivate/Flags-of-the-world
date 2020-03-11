157e60d051d51fb2245ca11d2da0be09291c1196
saadiyahhPrivate <saadiyah@mit.edu>
2020-03-11 04:35:57 -0400

    basic map with zooming ability. The color mappings are to help diagnoze missing data (details in css).

--- static/main.js
+++ static/main.js
@@ -1,39 +1,71 @@
-const width = 800,
-	height = 500;
-const svg = d3.select('div').append('svg')
+const width = 600,
+	    height = 400
+			scale0 = (width - 1) / 2 / Math.PI;
+
+const svg = d3.select('#map').append('svg')
 	.attr('width', width)
 	.attr('height', height);
+const countryName = d3.select('#countryname');
 
 function createMap(data) {
-	console.log('createMap');
+    var projection = d3.geoMercator()
+        .scale(300)
+        .translate([width / 2, height / 1.5]);
 
-	// Dummy code
-	// Currently, randomly generates a dot for each entry.
-	// Hover over the dot to see name of the entry
-	
-	svg.append('text')
-		.attr('id', 'countryname')
-		.attr('x', 20)
-		.attr('y', 20)
-		.attr('fill', 'blue')
-		.text('Choose a dot!')
+    var path = d3.geoPath()
+  							 .projection(projection);
 
-	const g = svg.selectAll('g')
-		.data(data.features)
-		.enter()
-		.append('g')
-		.attr('transform', 'translate(50,50)')
+		var zoom = d3.behavior.zoom()
+		.scaleExtent([0.3, 10])
+    .on("zoom",function() {
+        g.attr("transform","translate("+
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("path")
+            .attr("d", path.projection(projection));
+    });
 
+		countryName.append('text')
+			.attr('id', 'countryname')
+			.attr('x', 20)
+			.attr('y', 20)
+			.attr('fill', 'blue')
+			.text('Country Name!')
 
-	g.append('circle')
-		.attr('cx', d => Math.random() * (width-100))
-		.attr('cy', d => Math.random() * (height-100))
-		.attr('r', 5)
-		.attr('fill', 'blue')
-		.on('mouseover', function(d,i) {
-			svg.select('#countryname')
-				.text(d.properties.Name);
-		})
+		svg.attr("preserveAspectRatio", "xMidYMid")
+       .attr("viewBox", "0 0 " + width + " " + height);
 
-	//TODO
-}
+		var view = svg.append("rect")
+			 .attr("class", "background")
+			 .attr("width", width)
+			 .attr("height", height);
+			 // .on("click", country_clicked); to be added later
+
+		function checkProperty(d) {
+			if (d.properties.Name == null && d.properties.ImageURL == null) {
+				return "misingbothcountry";
+		  } else if (d.properties.Name == null) {
+				return "nodatacountry";
+			} else if (d.properties.ImageURL == null) {
+				return "noflagcountry";
+			} else {
+				return "countries";
+			}
+		};
+
+		var g = svg.append("g");
+		svg.selectAll("g")
+		 .append("g")
+		 .selectAll("path")
+		 .data(topojson.feature(data, data.objects.merged_countries).features)
+		 .enter()
+		 .append("path")
+		 .attr("id", function(d) { return d.id; })
+		 .attr("d", path)
+		 .on('mouseover', function(d,i) {
+			d3.select('#countryname')
+				.text(d.properties.ADMIN);
+			})
+			.attr("id", d => checkProperty(d));
+
+			svg.call(zoom);
+};
